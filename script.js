c28af6baf86ff2c145cb56430dc58dd17dcd4f19
wjcollier <williamjcollier4@gmail.com>
2022-08-04 08:38:36 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //   When there is no input
+  if (!guess) {
+    displayMessage('🎉 Correct Number!');
+    
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('⛔️ No Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#0096FF';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('🤡 you lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// start again
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#31087B';
+  document.querySelector('.number').style.width = '15rem';
+});
